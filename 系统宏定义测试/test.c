0250b5bfc3e9ec54dbbf82a8c32a83ad5ffd32eb
wanglinhuiyong <1585006673@qq.com>
2020-06-02 21:23:23 +0800

    Create test.c

---
+++ 系统宏定义测试/test.c
@@ -0,0 +1,63 @@
+void test()
+{
+	int len=sizeof(int)*8;
+	printf("sizeof(int)=%d\n",len);
+ 
+	len=sizeof(int *)*8;
+	printf("sizeof(int*)=%d\n",len);
+ 
+#ifdef _MSC_VER
+	printf("_MSC_VER is defined\n");
+#endif
+ 
+#ifdef __GNUC__
+	printf("__GNUC__ is defined\n");
+#endif
+ 
+#ifdef __INTEL__ 
+	printf("__INTEL__  is defined\n");
+#endif
+ 
+#ifdef __i386__ 
+	printf("__i386__  is defined\n");
+#endif
+ 
+#ifdef __x86_64__ 
+	printf("__x86_64__  is defined\n");
+#endif
+ 
+#ifdef _WIN32 
+	printf("_WIN32 is defined\n");
+#endif
+ 
+#ifdef _WIN64 
+	printf("_WIN64 is defined\n");
+#endif
+ 
+ 
+#ifdef __linux__ 
+	printf("__linux__ is defined\n");
+#endif
+ 
+#ifdef __LP64__ 
+	printf("__LP64__ is defined\n");
+#endif
+
+#ifdef __amd64 
+	printf("__amd64 is defined\n");
+#endif
+
+#if defined(__x86_64__) //x86-64位
+  printf("__x86_64__\n");
+#endif
+
+#if defined(__aarch64__) //arm-64位
+  printf("__aarch64__\n");
+#endif
+}
+ 
+int main(int argc, char* argv[])
+{
+	test();
+	return 0;
+}
